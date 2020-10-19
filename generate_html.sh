f8a17c1aa74937601b5f5cbf67f431a6684d2064
St John Karp <contact@stjohnkarp.net>
2020-10-19 15:11:13 -0500

    Fix some block-level elements
    Some Markdown generators don't recognize newer block-level elements
    and try to wrap them in <p> tags. Some sed commands will undo that.

--- generate_html.sh
+++ generate_html.sh
@@ -3,5 +3,12 @@
 echo "$1"
 
 swipl --traditional --quiet -l parse_entry.pl -g "consult('$2/site.pl'), generate_entry('$2/source/$1')." |
+	# Unwrap block-level elements that have erroneously been wrapped in <p> tags.
+	sed "s|<p><details|<details|g" |
+	sed "s|</summary></p>|</summary>|g" |
+	sed "s|<p></details></p>|</details>|g" |
+	sed "s|<p><figure|<figure|g" |
+	sed "s|</figure></p>|</figure>|g" |
+	# Smarten punctuation.
 	smartypants \
 	> "$2/output/${1%%.md}.html"
