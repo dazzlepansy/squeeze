683dc15379fb74dd9d81b341406128eacb96bbce
St John Karp <contact@stjohnkarp.net>
2020-10-19 16:48:27 -0500

    Key off the ID of the article date
    Removed a few lines of code from the RSS generation by keying off
    the ID of the article date element instead of looking for the Markdown
    files and then figuring out where the HTML is.

--- squeeze.sh
+++ squeeze.sh
@@ -18,16 +18,13 @@ find "$SOURCE_PATH" -type f -name "*.md" -printf "%P\0" |
 # Generate the RSS feed.
 mkdir -p "$OUTPUT_PATH/feeds"
 # Grep the date of each article.
-ARTICLES=$(grep --recursive --include "*.md" "^Date: " "$SOURCE_PATH" |
+ARTICLES=$(grep --recursive --include "*.html" "id=\"article-date\"" "$OUTPUT_PATH" |
 	# Sort articles by date (skipping the first field).
 	sort +1 |
 	# Get the last (i.e. most recent) posts for the RSS feed.
 	tail -5 |
 	# Reformat to just the file names.
 	cut --fields 1 --delimiter : |
-	# Convert paths so we operate on the generated HTML, not the unformatted Markdown.
-	sed "s|^$SOURCE_PATH|$OUTPUT_PATH|" |
-	sed 's|.md$|.html|' |
 	# Glue the file names together to be passed to Prolog.
 	paste --serial --delimiters ',' - |
 	sed "s|,|','|g")
